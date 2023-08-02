44689fcdb70360c928e8c7f5144f1444d4222416
jasonpyau <jasonyau101@gmail.com>
2023-08-02 08:50:04 -0400

    Final Changes

--- reflection/index.php
+++ reflection/index.php
@@ -11,21 +11,103 @@
         include('../include/header.php');
     ?>
     
-    <div class="p-4 bg-dark text-white" style="margin-top: 50px;">
-        <div>
-            Test
-        </div>
-        <div>
-            Test
-        </div>
-        <div>
-            Test
-        </div>
-        <div>
-            Test
-        </div>
-        <div>
-            Test
+    <div class="py-4 bg-dark text-white" style="margin-top: 50px;">
+        <div class="w-75 mx-auto">
+
+            <div class="fs-1 fw-bold text-decoration-underline text-center">
+                Reflection on my Progress as a Writer/Researcher this Semester
+            </div>
+
+            <hr>
+
+            <!-- Course Outcomes  -->
+            <div class="fs-2 fw-bold text-decoration-underline text-center">
+                Course Outcomes
+            </div>
+            <ol>
+                <li class="fs-4">
+                    <u>Generating Inquiry:</u>
+                    <ul>
+                        <li class="fs-5">
+                            In Task 2, I had to read, understand, and review other people's research and interview questions,
+                            then create my own research questions and interview questions that would help
+                            answer those research questions.
+                        </li>
+                    </ul>
+                </li>
+                <li class="fs-4">
+                    <u>Multiple Ways of Writing:</u>
+                    <ul>
+                        <li class="fs-5">
+                            In Task 3, I coded a simple website that includes a summary of what I learned in this class, how I improved 
+                            my writing and research skills, and how I will apply the learned knowledge in the future. I created
+                            a remediation of Task 2 by creating a Google Slides that summarizes the main points I made in that essay. Also,
+                            in Task 2, I complimented the writer for using visual charts and tables to summarize the answers in the interview
+                            study.
+                        </li>
+                    </ul>
+                </li>
+                <li class="fs-4">
+                    <u>Information Literacy:</u>
+                    <ul>
+                        <li class="fs-5">
+                            In Task 2, I used Google Scholar to find appropriate and relevant research papers that were from scholarly sources 
+                            such as Carnegie Mellon University and the University of San Diego. I also made sure to use scholarly articles in Task
+                            1 published by universities and NASA.
+                        </li>
+                    </ul>
+                </li>
+                <li class="fs-4">
+                    <u>Research Genre Production:</u>
+                    <ul>
+                        <li class="fs-5">
+                            In Task 2, my research community is related to computer science, and I made sure to use the correct terminology; for example, 
+                            in software engineering, a "bug" means an error in a program, which is different than the meaning outside the programming community.
+                            In Task 3, I made sure to define key terms at the beginning of the Google Slides so the reader would understand the basic terminology
+                            used.
+                        </li>
+                    </ul>
+                </li>
+                <li class="fs-4">
+                    <u>Contributing Knowledge:</u>
+                    <ul>
+                        <li class="fs-5">
+                            In Task 2, I pointed out successes in a research study, such as the use of visual charts to summarize data, but also pointed out some missing 
+                            pieces, such as the lack of listing the types of projects each interviewee contributed to and a sample population that wasn’t general 
+                            enough. 
+                        </li>
+                    </ul>
+                </li>
+                <li class="fs-4">
+                    <u>Revision:</u>
+                    <ul>
+                        <li class="fs-5">
+                            In Task 3, I created a remediation to Task 2 by creating a Google Slides to shorten the content for readers who want a quick and concise
+                            summary of my essay. In Tasks 2 and 1, I listened to the advice of my peers when they said to consider counterpoints, and I helped
+                            provide meaningful feedback to my peers. I made sure to proofread all my work and fix any grammatical mistakes before submitting.
+                        </li>
+                    </ul>
+                </li>
+            </ol>
+
+            <hr>
+
+            <!-- Reflection -->
+            <div class="fs-2 fw-bold text-decoration-underline text-center">
+                Reflection
+            </div>
+            <div class="fs-5">
+                Before this class, I'd always struggled with writing in large quantities, as I would usually over-summarize my main points. I've learned that writing comes 
+                a lot more naturally to me when I have a genuine interest in the topic. I've improved my ability to write and research in many ways. I learned how to use Google 
+                Scholar and Primo Search with keywords like 'OR' and 'AND' to find the most relevant scholarly articles. I think this will be a very useful skill to have in the 
+                future, since there's so much information to learn on the Internet, and being able to search for credible sources will help you find that information. I've learned 
+                how to read, understand, and critique research papers. I've noticed that most of them share a similar format: they start with a background and related work, propose 
+                research questions and methods, state and discuss findings, discuss possible limitations, and write a brief conclusion. I improved my skills in briefly summarizing 
+                my main ideas and points attractively in a PowerPoint or Google Slides and creating websites to share information with the public. This will obviously be a useful 
+                skill to have as a software engineer, as I will have to write documentation that briefly summarizes my code, and I might have to work on front-end (what the end-user 
+                will visually see) development.
+            </div>
+
         </div>
     </div>
 
--- remediation/index.php
+++ remediation/index.php
@@ -15,7 +15,7 @@
         <u class="fs-1 fw-bold pb-3">
             Task 2, Part 1 Remediation
         </u>
-        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQsZbb9Xm-cBOICyEJr9dPIN4F58x1Y4P1UnGyhWvGLB5H47dTXCsJMVf1w3M9dqb78GFSq4dMBcGNs/embed?start=false&loop=true&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" class="vh-100 w-75"></iframe>
+        <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQsZbb9Xm-cBOICyEJr9dPIN4F58x1Y4P1UnGyhWvGLB5H47dTXCsJMVf1w3M9dqb78GFSq4dMBcGNs/embed?start=false&loop=true&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" width="80%" class="vh-100"></iframe>
         <a href="https://docs.google.com/presentation/d/e/2PACX-1vQsZbb9Xm-cBOICyEJr9dPIN4F58x1Y4P1UnGyhWvGLB5H47dTXCsJMVf1w3M9dqb78GFSq4dMBcGNs/embed?start=false&loop=true&delayms=3000" target="_blank">[Link]</a>
     </div>
 
